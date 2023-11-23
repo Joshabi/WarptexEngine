b488aa91bb746cef2a971fa81e97adc17d662409
Joshabi <joshuawright712@hotmail.co.uk>
2023-11-23 23:35:19 +0000

    Remerged CPP back into Header
    I prefer it this way honestly

--- Warptex/ColliderComponent.h
+++ Warptex/ColliderComponent.h
@@ -17,37 +17,50 @@ enum class CollisionLayer {
     OBSTACLE = 4
 };
 
+/// <summary>
+/// Context pertaining to the current collisions of a collider
+/// </summary>
 class CollisionContext {
 public:
-    CollisionContext(class ColliderComponent* parent);
-    ~CollisionContext();
+    CollisionContext(class ColliderComponent* parent) { parentCollider = parent; }
+    ~CollisionContext() { }
 
-    std::vector<GObject*> GetCollisions();
+    std::vector<GObject*> GetCollisions() { return currentCollisions; }
 
     template<typename T>
-    std::vector<T*> GetCollisionsOfType();
+    std::vector<T*> GetCollisionsOfType() {
+        std::vector<T*> collisions;
+        return collisions;
+    }
 
-    void UpdateCollisions(std::vector<GObject*> objectsToCheck);
+    void UpdateCollisions(std::vector<GObject*> objectsToCheck) { }
 
 private:
     std::vector<GObject*> currentCollisions;
     class ColliderComponent* parentCollider;
 };
 
+/// <summary>
+/// Handles collisions for its parent GameObject, representing a hitbox
+/// </summary>
 class ColliderComponent : public Component {
 public:
     SDL_Rect collider;
     CollisionLayer colLayer;
 
+    ColliderComponent(CollisionLayer colLayer) {
+        this->colLayer = colLayer;
+        Init();
+    }
+    ~ColliderComponent() { }
+
+    void Init() override { }
+    void Update() override { }
+    void UpdateCollisions(std::vector<GObject*> objectsToCheck) {
+        colContext->UpdateCollisions(objectsToCheck);
+    }
+
 private:
     TransformComponent* transform;
     CollisionContext* colContext;
-
-public:
-    ColliderComponent(CollisionLayer colLayer);
-    ~ColliderComponent();
-
-    void Init() override;
-    void Update() override;
-    void UpdateCollisions(std::vector<GObject*> objectsToCheck);
 };
